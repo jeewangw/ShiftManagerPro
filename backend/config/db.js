1085e77138d7ac925b2ff03d6760a0c675cf8d2f
Jeevan <jeewangw9@gmail.com>
2026-05-15 22:59:43 -0500

    feat(database): update credentails

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:               process.env.DB_HOST     || 'mysql.railway.internal',
-  port:               parseInt(process.env.DB_PORT || '3306'),
+  host:               process.env.DB_HOST     || 'tramway.proxy.rlwy.net',
+  port:               parseInt(process.env.DB_PORT || '47017'),
   user:               process.env.DB_USER     || 'root',
   password:           process.env.DB_PASSWORD || 'SpdgawFyGUIJlAShCPZpiEytQnrMPRSY',
   database:           process.env.DB_NAME     || 'railway',
